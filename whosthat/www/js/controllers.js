d03865e34c1bd93cf8a6454c52e4869e2f6ccf24
Nattk <Kifoyinattan@gmail.com>
2017-03-20 00:01:27 +0100

    clean app

--- whosthat/www/js/controllers.js
+++ whosthat/www/js/controllers.js
@@ -1,6 +1,6 @@
-angular.module('starter.controllers', ['ngCordova'])
+angular.module('whosthat.controllers', ['ngCordova'])
 
-.controller('DashCtrl', function($scope,$cordovaCamera) {
+.controller('HomeCtrl', function($scope,$cordovaCamera) {
 
   $scope.takePhoto = function () {
                  var options = {
@@ -20,30 +20,11 @@ angular.module('starter.controllers', ['ngCordova'])
                    }, function (err) {
                        // An error occured. Show a message to the user
                    });
-               }
+  }
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+.controller('LoadingCtrl', function($scope,$cordovaCamera) {
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('ResultsCtrl', function($scope,$cordovaCamera) {
 });
