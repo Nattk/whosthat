02776474359ea1412eac8e8fd29b1caa6edc1ccb
Nattk <Kifoyinattan@gmail.com>
2017-03-19 23:38:36 +0100

    add ng-cordova and controller for the photo plugin

--- whosthat/www/js/app.js
+++ whosthat/www/js/app.js
@@ -11,8 +11,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    document.addEventListener("deviceready", onDeviceReady, false);
-
 
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
--- whosthat/www/js/controllers.js
+++ whosthat/www/js/controllers.js
@@ -1,6 +1,27 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ngCordova'])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope,$cordovaCamera) {
+
+  $scope.takePhoto = function () {
+                 var options = {
+                   quality: 75,
+                   destinationType: Camera.DestinationType.DATA_URL,
+                   sourceType: Camera.PictureSourceType.CAMERA,
+                   allowEdit: false,
+                   encodingType: Camera.EncodingType.JPEG,
+                   targetWidth: 300,
+                   targetHeight: 300,
+                   popoverOptions: CameraPopoverOptions,
+                   saveToPhotoAlbum: true
+               };
+
+                   $cordovaCamera.getPicture(options).then(function (imageData) {
+                       $scope.imgURI = "data:image/jpeg;base64," + imageData;
+                   }, function (err) {
+                       // An error occured. Show a message to the user
+                   });
+               }
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
