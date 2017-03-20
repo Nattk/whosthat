bfd563ebdaf78fd9983396950e5b1c169e092ba3
cenart <tuna.dylan@gmail.com>
2017-03-20 14:38:26 +0100

    ajout du service loading

--- whosthat/www/js/controllers.js
+++ whosthat/www/js/controllers.js
@@ -1,74 +1,15 @@
 angular.module('whosthat.controllers', ['ngCordova'])
 
-.controller('HomeCtrl', function($scope,$cordovaCamera,$cordovaFileTransfer,$ionicLoading,$q) {
+.controller('HomeCtrl', function($scope,loadingService,PhotoFactory) {
+  loadingService.show();
 
- $scope.show = function() {
-  $ionicLoading.show({
-   templateUrl: 'templates/loading.html',
- }).then(function(){
-   console.log("The loading indicator is now displayed");
-   $scope.hide();
- });
-};
-$scope.show();
-$scope.hide = function(){
-  $ionicLoading.hide().then(function(){
-   console.log("The loading indicator is now hidden");
- });
-};
-$scope.uploadPhoto = function(imageURI){
-  document.addEventListener('deviceready', function (imageURI) {
-    var server = "http://shazoom.alwaysdata.net/upload/upload.php";
-    $cordovaFileTransfer.upload(server, imageURI, options)
-    .then(function(result) {
-        // Success!
-        alert('Success');
-      }, function(err) {
-        alert('errorUploade');
-      }, function (progress) {
-        // constant progress updates
+  ionic.Platform.ready(function(){
+      // will execute when device is ready, or immediately if the device is already ready.
+      loadingService.hide();
+});
+  PhotoFactory.takePhoto().then(function(response){
+        PhotoFactory.uploadPhoto(response);
       });
-
-  }, false);
-};
-$scope.getPhoto = function(){
-   var deferred = $q.defer();
-   $cordovaCamera.getPicture(options).then(function (imageURI) {
-     var image = document.getElementById('myImage');
-
-   }, function (err) {
-     alert('error');
-   });
-};
-$scope.takePhoto = function () {
-
- var options = {
-   quality: 75,
-   destinationType: Camera.DestinationType.FILE_URI,
-   sourceType: Camera.PictureSourceType.CAMERA,
-   allowEdit: false,
-   encodingType: Camera.EncodingType.JPEG,
-   targetWidth: 300,
-   targetHeight: 300,
-   popoverOptions: CameraPopoverOptions,
-   saveToPhotoAlbum: true
- };
-  $scope.getPhoto().then(function(response){
-    deferred.resolve(response)
-    alert(response);
-   // $scope.uploadPhoto(imageURI);
-  },
-  function(response){
-    deferred.reject(response);
-    alert ('error');
-  });
-  return deferred.promise;
-
-}
-
-
-
-
 
 })
 
--- whosthat/www/js/services.js
+++ whosthat/www/js/services.js
@@ -1,1 +1,18 @@
-angular.module('whosthat.services', [])
+angular
+.module('whosthat.services', ['ngCordova'])
+.service('loadingService', loadingService);
+
+function loadingService($ionicLoading){
+	this.show = function() {
+		$ionicLoading.show({
+			templateUrl: 'templates/loading.html',
+		}).then(function(){
+			console.log("The loading indicator is now displayed");
+		});
+	};
+	this.hide = function(){
+		$ionicLoading.hide().then(function(){
+			console.log("The loading indicator is now hidden");
+		});
+	};
+}
