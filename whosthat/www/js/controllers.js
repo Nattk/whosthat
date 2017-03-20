610accabbbb93001614477d2352759c899d4df20
Nattk <Kifoyinattan@gmail.com>
2017-03-20 14:38:27 +0100

    services photos, error upload

--- whosthat/www/js/controllers.js
+++ whosthat/www/js/controllers.js
@@ -1,75 +1,11 @@
-angular.module('whosthat.controllers', ['ngCordova'])
+angular.module('whosthat.controllers', ['ngCordova',])
 
-.controller('HomeCtrl', function($scope,$cordovaCamera,$cordovaFileTransfer,$ionicLoading,$q) {
-
- $scope.show = function() {
-  $ionicLoading.show({
-   templateUrl: 'templates/loading.html',
- }).then(function(){
-   console.log("The loading indicator is now displayed");
-   $scope.hide();
- });
-};
-$scope.show();
-$scope.hide = function(){
-  $ionicLoading.hide().then(function(){
-   console.log("The loading indicator is now hidden");
- });
-};
-$scope.uploadPhoto = function(imageURI){
-  document.addEventListener('deviceready', function (imageURI) {
-    var server = "http://shazoom.alwaysdata.net/upload/upload.php";
-    $cordovaFileTransfer.upload(server, imageURI, options)
-    .then(function(result) {
-        // Success!
-        alert('Success');
-      }, function(err) {
-        alert('errorUploade');
-      }, function (progress) {
-        // constant progress updates
+.controller('HomeCtrl', function($scope,PhotoFactory) {
+  $scope.tout= function(){
+    PhotoFactory.takePhoto().then(function(response){
+        PhotoFactory.uploadPhoto(response);
       });
-
-  }, false);
-};
-$scope.getPhoto = function(){
-   var deferred = $q.defer();
-   $cordovaCamera.getPicture(options).then(function (imageURI) {
-     var image = document.getElementById('myImage');
-
-   }, function (err) {
-     alert('error');
-   });
-};
-$scope.takePhoto = function () {
-
- var options = {
-   quality: 75,
-   destinationType: Camera.DestinationType.FILE_URI,
-   sourceType: Camera.PictureSourceType.CAMERA,
-   allowEdit: false,
-   encodingType: Camera.EncodingType.JPEG,
-   targetWidth: 300,
-   targetHeight: 300,
-   popoverOptions: CameraPopoverOptions,
-   saveToPhotoAlbum: true
- };
-  $scope.getPhoto().then(function(response){
-    deferred.resolve(response)
-    alert(response);
-   // $scope.uploadPhoto(imageURI);
-  },
-  function(response){
-    deferred.reject(response);
-    alert ('error');
-  });
-  return deferred.promise;
-
 }
-
-
-
-
-
 })
 
 
--- whosthat/www/js/services.js
+++ whosthat/www/js/services.js
@@ -1,1 +1,54 @@
-angular.module('whosthat.services', [])
+angular.module('whosthat.services', ['ngCordova'])
+
+.factory('PhotoFactory', function($http,$q,$cordovaCamera,$cordovaFileTransfer) {
+  const API_URL ="http://greenvelvet.alwaysdata.net/kwick/api/";
+
+  return {
+
+    takePhoto : function(){
+      var options = {
+        quality: 1,
+        destinationType: Camera.DestinationType.FILE_URI,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: false,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 300,
+        targetHeight: 300,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: true
+      };
+
+      return this.getPhoto(options);
+
+    },
+
+    getPhoto : function(options){
+      var deferred = $q.defer();
+      $cordovaCamera.getPicture(options).then(function(imageURI) {
+        deferred.resolve(imageURI);
+        //alert(imageURI);
+
+      }, function (imageURI) {
+        deferred.reject(imageURI)
+        alert('error');
+      });
+
+      return deferred.promise;
+    },
+
+    uploadPhoto : function(imageURI){
+      document.addEventListener('deviceready', function () {
+        alert(imageURI);
+        var server = "http://shazoom.alwaysdata.net/upload/upload.php";
+        $cordovaFileTransfer.upload(server, imageURI, options)
+        .then(function(result) {
+            alert('Success');
+          }, function(err) {
+            alert('errorUploade');
+          }, function (progress) {
+            // constant progress updates
+          });
+
+      }, false);
+    }
+}});
