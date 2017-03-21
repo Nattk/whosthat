a868cae48ed218e326fc8dec6c75eef9b8e5eae1
Nattk <Kifoyinattan@gmail.com>
2017-03-21 01:43:52 +0100

    Call Compare API

--- whosthat/www/js/controllers.js
+++ whosthat/www/js/controllers.js
@@ -1,19 +1,22 @@
 angular.module('whosthat.controllers', ['ngCordova',])
 
-.controller('HomeCtrl', function($scope,loadingService,PhotoFactory) {
-  loadingService.show();
-
-  ionic.Platform.ready(function(){
-      // will execute when device is ready, or immediately if the device is already ready.
-      loadingService.hide();
-});
+.controller('HomeCtrl', function($scope,PhotoFactory,FaceCompare) {
+//   loadingService.show();
+//
+//   ionic.Platform.ready(function(){
+//       // will execute when device is ready, or immediately if the device is already ready.
+//       loadingService.hide();
+// });
 $scope.tout= function(){
   PhotoFactory.takePhoto().then(function(response){
       PhotoFactory.uploadPhoto(response);
     });
 }
-})
 
+$scope.compare= function(){
+  FaceCompare.compare();
+}
+})
 
 .controller('ResultsCtrl', function($scope,$cordovaCamera,$cordovaFileTransfer) {
 });
--- whosthat/www/js/services.js
+++ whosthat/www/js/services.js
@@ -53,19 +53,43 @@ angular.module('whosthat.services', ['ngCordova'])
     }
 }})
 
-.service('loadingService', loadingService);
 
-function loadingService($ionicLoading){
-	this.show = function() {
-		$ionicLoading.show({
-			templateUrl: 'templates/loading.html',
-		}).then(function(){
-			console.log("The loading indicator is now displayed");
-		});
-	};
-	this.hide = function(){
-		$ionicLoading.hide().then(function(){
-			console.log("The loading indicator is now hidden");
-		});
-	};
-}
+.factory('FaceCompare', function($http,$q) {
+
+  const API_KEY = 'Vrw1X_hkh_ZLiP2CXuX3iUbJgY9Oe5Cs';
+  const API_SECRET = 'GBYvqVlQxdEbfGMeQBLmmoFFmTYsBgMN';
+  const API_URL = 'https://api-us.faceplusplus.com/facepp/v3/compare?api_key=';
+  return{
+
+    compare : function(url1,url2){
+      url1='https://pbs.twimg.com/profile_images/734121846275198976/jx-E4sWp_400x400.jpg';
+      url2='https://image.noelshack.com/fichiers/2015/32/1438766535-ryan-reynolds.jpg';
+
+      $http.post(API_URL + API_KEY + '&api_secret=' + API_SECRET + '&image_url1=' +url1+ '&image_url2='+url2 ).then(function(data){
+        console.log(data);
+      }, function(err){
+        console.log(err);
+      });
+    }
+
+  }
+
+});
+
+
+// .service('loadingService', loadingService);
+//
+// function loadingService($ionicLoading){
+// 	this.show = function() {
+// 		$ionicLoading.show({
+// 			templateUrl: 'templates/loading.html',
+// 		}).then(function(){
+// 			console.log("The loading indicator is now displayed");
+// 		});
+// 	};
+// 	this.hide = function(){
+// 		$ionicLoading.hide().then(function(){
+// 			console.log("The loading indicator is now hidden");
+// 		});
+// 	};
+// }
