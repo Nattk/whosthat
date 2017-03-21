c3b9fa755dc1a559f9f696664a977abef40091db
cenart <tuna.dylan@gmail.com>
2017-03-21 21:53:40 +0100

    ajout des données a la vue dans result.html

--- whosthat/www/js/controllers.js
+++ whosthat/www/js/controllers.js
@@ -1,12 +1,12 @@
 angular.module('whosthat.controllers', ['ngCordova',])
 
-.controller('HomeCtrl', function($scope,loadingService,PhotoFactory,WikiFactory) {
+.controller('HomeCtrl', function($scope,$rootScope,$state,loadingService,PhotoFactory,WikiFactory) {
   loadingService.show();
 
   ionic.Platform.ready(function(){
       // will execute when device is ready, or immediately if the device is already ready.
       loadingService.hide();
-});
+  });
 $scope.tout= function(){
   PhotoFactory.takePhoto().then(function(response){
       PhotoFactory.uploadPhoto(response);
@@ -14,10 +14,12 @@ $scope.tout= function(){
 }
 
 $scope.name = "Jean Dujardin";
-
+$scope.infos = {};
 $scope.wiki= function(){
   WikiFactory.get($scope.name).then(function(response){
-      console.log(response);
+      $rootScope.infos = response;
+      console.log($rootScope.infos)
+      $state.go('results');
     });
 }
 
@@ -25,5 +27,11 @@ $scope.wiki= function(){
 
 
 
-.controller('ResultsCtrl', function($scope,$cordovaCamera,$cordovaFileTransfer) {
+.controller('ResultsCtrl', function($scope,$cordovaCamera,$rootScope,$cordovaFileTransfer) {
+  $scope.openLink = function(){
+    $scope.articleName = $rootScope.infos.title.replace(' ','_');
+    $scope.link = "https://fr.wikipedia.org/wiki/"+$scope.articleName;
+    console.log($scope.link)
+    window.open($scope.link, '_system', 'location=yes ');
+  }
 });
